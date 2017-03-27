d8bb3b80641007d1c61a8639160be1cc69ae392e
ian <me@iany.me>
2017-03-27 17:55:48 +0800

    initial commit

---
+++ Assets/Scripts/BackgroundLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundLoader : MonoBehaviour {
+	public Texture2D texture;
+
+	private float ratio;
+
+	void Start() {
+		GetComponent<Renderer>().material.mainTexture = texture;
+		ratio = texture.texelSize.y / texture.texelSize.x;
+	}
+
+	void Update() {
+		var scale = Vector3.one;
+		var screenRatio = (float)Screen.width / Screen.height;
+		Debug.Log("screen ratio = " + screenRatio);
+		if (screenRatio > ratio) {
+			scale.x = Camera.main.orthographicSize * 2 * Screen.width / Screen.height;
+			scale.y = scale.x / ratio;
+		} else {
+			scale.y = Camera.main.orthographicSize * 2;
+			scale.x = scale.y * ratio;
+		}
+
+		transform.localScale = scale;
+	}
+}
---
+++ Assets/Scripts/MotionsPlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using live2d;
+
+public class MotionsPlayer : MonoBehaviour {
+	public SimpleModel model;
+	public TextAsset[] motionFiles;
+
+	private MotionQueueManager motionMgr;
+	private Live2DMotion[] motions;
+	private int currentMotionIndex = 0;
+
+	void Start() {
+		motionMgr = new MotionQueueManager();
+		motions = new Live2DMotion[motionFiles.Length];
+		for (int i = 0; i < motionFiles.Length; i++) {
+			motions[i] = Live2DMotion.loadMotion(motionFiles[i].bytes);
+		}
+
+		if (model == null) {
+			model = GetComponent<SimpleModel>();
+		}
+	}
+	
+	void Update() {
+		if (motions == null || motions.Length == 0) {
+			return;
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			var motion = motions[currentMotionIndex];
+			currentMotionIndex++;
+			if (currentMotionIndex >= motions.Length) {
+				currentMotionIndex = 0;
+			}
+			motionMgr.stopAllMotions();
+			motionMgr.startMotion(motion);
+		}
+
+		motionMgr.updateParam(model.model);
+	}
+}
